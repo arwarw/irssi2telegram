fbe3caf8c4a01a80267e994be9f604056a1ed93b
Alexander Wuerstlein <arw@arw.name>
2016-07-12 00:23:02 +0200

    trivialer hallo-arw-bot

---
+++ test.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Slurp;
+use WWW::Telegram::BotAPI;
+use Data::Dumper qw(Dumper);
+use v5.18;
+
+my $token = read_file("./token");
+chomp($token);
+my $api = WWW::Telegram::BotAPI->new(token => $token);
+my $me = $api->getMe or die "could not getMe";
+say "I am ". $me->{user}{name};
+
+my $updates;
+my $offset;
+while (1) {
+	$updates = $api->getUpdates({timeout => 30, $offset?(offset => $offset):()});
+	unless ($updates and ref $updates eq "HASH" and $updates->{ok}) {
+		warn "updates weird";
+		next;
+	}
+	for my $u (@{$updates->{result}}) {
+		 $offset = $u->{update_id} + 1 if $u->{update_id} >= $offset;
+		 say "Message from " . $u->{message}{from}{username};
+		 say Dumper($u);
+
+		 $api->sendMessage({
+			 	chat_id => $u->{message}{chat}{id},
+				text => "Hallo arw",
+			 });
+	}
+}
